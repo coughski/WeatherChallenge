e3c1b75efcb1920c8266f265ff88a454d14f77a8
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2023-05-18 17:13:32 -0400

    Initial Commit

---
+++ WeatherChallenge/WeatherChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherChallengeApp.swift
+//  WeatherChallenge
+//
+//  Created by Kuba Szulaczkowski on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
